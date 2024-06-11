c9767f77919353cdfd739799934e5ac080f93c9f
Veritaaaas <rodneyjamesmmaisog@iskolarngbayan.pup.edu.ph>
2024-06-11 13:59:00 +0800

    removed the baggage weights attribute

--- server/index.js
+++ server/index.js
@@ -1,24 +1,27 @@
-
 const express = require('express');
 const googleMapsClient = require('@google/maps').createClient({
   key: 'AIzaSyDe_-D5V_ilS9ejhdFVuM6WQPdCmQexZzw'
 });
-
 const cors = require('cors');
 const app = express();
 
-app.use(cors()); 
+app.use(cors());
 
 app.get('/route', (req, res) => {
+    // Get locations and weights from the query parameters
     const locations = req.query.locations.split('|');
 
+    // Request distance matrix from Google Maps API
     googleMapsClient.distanceMatrix({
         origins: locations,
         destinations: locations,
         mode: 'driving'
     }, (err, response) => {
         if (!err) {
+            // Parse the distance matrix
             const distanceMatrix = response.json.rows.map(row => row.elements.map(element => element.distance.value));
+            
+            // Send the distance matrix back to the client
             res.json(distanceMatrix);
         } else {
             console.error(err);
